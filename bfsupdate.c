65cd6f8c389471410e42829e4bdd9e93f3cf9479
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-08-01 10:19:43 +0530

    Create bfsupdate.c

---
+++ bfsupdate.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+int i, n, j, count = 0, opc, visit[10], acyclic = 1, f = 0, r = -1;
+int a[10][10], traverse[10][10], queue[10];
+void bfs(int s)
+{
+    int k = 1, p[n];
+    count++;
+    queue[++r] = s;
+    p[s] = 0;
+    visit[s] = 0;
+    while (f <= r)
+    {
+        s = queue[f++];
+        visit[s] = 1;
+        traverse[count][k++] = s;
+        for (i = 1; i <= n; i++)
+        {
+            opc++;
+            if (a[s][i] && visit[i] == 1 && p[s] != i)
+                acyclic = 0;
+            if (a[s][i] && visit[i] == 0)
+            {
+                queue[++r] = i;
+                p[i] = s;
+            }
+        }
+    }
+}
+void connectandcyclic()
+{
+    int flag = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (visit[i] == 1)
+            visit[i] = 2;
+        if (visit[i] == 0)
+        {
+            flag = 0;
+            bfs(i);
+        }
+    }
+    if (flag)
+    {
+        printf("Graph is Connected\n");
+        for (j = 1; j <= n; j++)
+            printf("%d ", traverse[1][j]);
+    }
+    else
+    {
+        printf("Graph is Disconnected\n");
+        printf("Number of Connected Components : %d\n", count);
+        printf("They are\n");
+        for (i = 1; i <= count; i++)
+        {
+            printf("Component-%d : ", i);
+            for (j = 1; j <= n; j++)
+                if (traverse[i][j] != 0)
+                    printf("%d ", traverse[i][j]);
+            printf("\n");
+        }
+    }
+    if (acyclic)
+        printf("\nGraph is Acyclic\n");
+    else
+        printf("\nGraph is Cyclic\n");
+}
+int main()
+{
+    int start;
+    FILE *fp;
+    fp=fopen("bfs.txt","a");
+    printf("Enter the number of nodes : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        visit[i] = 0;
+    printf("Enter the adjacency matrix\n");
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            scanf("%d", &a[i][j]);
+    printf("Enter the Starting Vertex : ");
+    scanf("%d", &start);
+    opc = 0;
+    count = 0;
+    bfs(start);
+    connectandcyclic();
+    printf("Operation Count : %d\n", opc);
+    fprintf(fp,"%d\t %d\n",n,opc);
+}
